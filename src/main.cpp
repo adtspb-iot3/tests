9be20b5003f3be9d0fccccc8151de7da1eb855da
adtspb-iot3 <79463276+adtspb-iot3@users.noreply.github.com>
2021-02-26 20:54:36 +0300

    Работает с замечаниями

--- src/main.cpp
+++ src/main.cpp
@@ -1,70 +1,74 @@
 #include <Arduino.h>
 #include "timer.h"
 #include "knopka.h"
+#define TIME_SHORT 600
+#define TIME_DOUBLE 500
 // bool volatile button1 {};
 bool flag{0};
-const int PIN {14};
+const int PIN {D3};
 //*************
 int pin{PIN};
 
-enum Stats{
+enum class Stats{
+  NONE,
   SHORT_PRESS,
   LONG_PRESS,
   DOUBLE_PRESS
 };
-Stats sost;
+Stats volatile sost{};
 //***********************
 // Knopka knopka (PIN, &flag);
-  long int volatile t1{ };
-   long int volatile t2{ };
-   long int volatile t3{ };
-   int volatile t_o{1200};
+unsigned long volatile t1{};
+unsigned long volatile t2{};
+unsigned long volatile t3{};
+unsigned long volatile t_o{};
 //********************************
-   bool button_down{};
+bool button_down{};
 //***************************
+bool getShort(){
+  return t_o > millis();
+}
+//---------------------------
 void push_button_down();
-void push_button_up(){
-     static unsigned long millis_prev1;
-    if (millis() - 200 > millis_prev1)
-    {
-          millis_prev1 = millis();
-          t2 = millis();
-            if(t2 - t1>=t_o)
-            {
-            sost=LONG_PRESS;
-            t3=t1;
-            Serial.println( sost);
-            }
-   else if(t2-t1<t_o)
-   {
-       sost=SHORT_PRESS;
-       Serial.println( sost);
-   }
+void ICACHE_RAM_ATTR push_button_up(){
+  static unsigned long millis_prev_up;
+  if (millis() - 200 > millis_prev_up)
+  {
+    t2 = millis_prev_up = millis();
+    if(t2 - t1 >= TIME_SHORT) {
+      sost = Stats::LONG_PRESS;
     }
-         attachInterrupt(digitalPinToInterrupt(pin), push_button_down, FALLING);
-    
-};
-void push_button_down(){
-        static unsigned long millis_prev;
-    if (millis() - 200 > millis_prev){
-        t1 = millis();
-        // if(t1-t3>1000)
-        button_down=true;
-        millis_prev = millis();
+     else {
+      if( t3 > 0 )
+        sost = Stats::SHORT_PRESS;
     }
-        attachInterrupt(digitalPinToInterrupt(pin), push_button_up, RISING);
-    
+  }
+  attachInterrupt(digitalPinToInterrupt(pin), push_button_down, FALLING);
+}
+void ICACHE_RAM_ATTR push_button_down(){
+  static unsigned long millis_prev;
+  if (millis() - 200 > millis_prev){
+    millis_prev = t1 = millis();
+    if( t1 - t3 < TIME_DOUBLE){
+      sost = Stats::DOUBLE_PRESS;
+      t2 = t3 = 0;
+    } else t3 = t1;
+  }
+  attachInterrupt(digitalPinToInterrupt(pin), push_button_up, RISING);
 };
 //*******************
 void setup(){
   Serial.begin(115200);
   pinMode(PIN,INPUT_PULLUP);
-
+  attachInterrupt(digitalPinToInterrupt(pin), push_button_down, FALLING);
 }
  
 void loop() {
-
-//Serial.println(digitalRead(PIN));
+  // Serial.println(digitalRead(PIN));
+  if(sost != Stats::NONE){
+    Serial.println( static_cast<int>(sost));
+    sost = Stats::NONE;
+  }
 }
  
 
