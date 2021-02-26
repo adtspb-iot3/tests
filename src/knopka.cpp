7a290acd68654322db981ad6187f881f33fbf998
adtspb-iot3 <79463276+adtspb-iot3@users.noreply.github.com>
2021-02-26 19:19:29 +0300

    Артем закончил

--- src/knopka.cpp
+++ src/knopka.cpp
@@ -1,49 +1,7 @@
-#include <Arduino.h>
-#include "knopka.h"
-bool volatile button1;
-
-//**************************
-void push_button(){
-     static unsigned long millis_prev;
-  if (millis() - 200 > millis_prev)
-    button1 = 1;
-   
-  millis_prev = millis();
-};
-void Knopka::cycle() {
-    switch(sost_sys){
-    case FREE: 
-        if(button1) { 
-            setTimer(1200); 
-            button1 = 0; 
-            sost_sys = FIRST_PRESS; 
-            Serial.println("FREE");
-        } 
-    break;
-    case FIRST_PRESS: 
-        if(getTimer()){
-            Serial.println("FIRST_PRESS");
-            if(digitalRead(pin))
-            {
-                Serial.println("LONG");
-                sost=LONG_PRESS;
-            }
-            else {
-                sost=SHOT_PRESS;
-                Serial.println("short");
-            }; 
-            sost_sys = FREE;
-            *flag = 1;
-        } else if(button1) {
-                sost_sys = FREE;
-                Serial.println("Double");
-                *flag=1;
-                sost=DOUBLE_PRESS;
-            }
-            else Serial.println("ELSE");
-     break;
-    };
-   
-
-
-}
+// #include <Arduino.h>
+// #include "knopka.h"
+// //***************************
+ 
+// void Knopka::cycle(){
+ 
+// }
--- src/main.cpp
+++ src/main.cpp
@@ -5,22 +5,66 @@
 bool flag{0};
 const int PIN {14};
 //*************
-Knopka knopka (PIN, &flag);
+int pin{PIN};
+
+enum Stats{
+  SHORT_PRESS,
+  LONG_PRESS,
+  DOUBLE_PRESS
+};
+Stats sost;
+//***********************
+// Knopka knopka (PIN, &flag);
+  long int volatile t1{ };
+   long int volatile t2{ };
+   long int volatile t3{ };
+   int volatile t_o{1200};
+//********************************
+   bool button_down{};
+//***************************
+void push_button_down();
+void push_button_up(){
+     static unsigned long millis_prev1;
+    if (millis() - 200 > millis_prev1)
+    {
+          millis_prev1 = millis();
+          t2 = millis();
+            if(t2 - t1>=t_o)
+            {
+            sost=LONG_PRESS;
+            t3=t1;
+            Serial.println( sost);
+            }
+   else if(t2-t1<t_o)
+   {
+       sost=SHORT_PRESS;
+       Serial.println( sost);
+   }
+    }
+         attachInterrupt(digitalPinToInterrupt(pin), push_button_down, FALLING);
+    
+};
+void push_button_down(){
+        static unsigned long millis_prev;
+    if (millis() - 200 > millis_prev){
+        t1 = millis();
+        // if(t1-t3>1000)
+        button_down=true;
+        millis_prev = millis();
+    }
+        attachInterrupt(digitalPinToInterrupt(pin), push_button_up, RISING);
+    
+};
 //*******************
 void setup(){
   Serial.begin(115200);
-  // pinMode(PIN,INPUT_PULLUP);
+  pinMode(PIN,INPUT_PULLUP);
 
 }
  
 void loop() {
-if(flag) {
-    int k = knopka.Whatsapp();
-    Serial.println(k);
-    flag = 0;
-  }
-  knopka.cycle();
 
+//Serial.println(digitalRead(PIN));
 }
  
 
